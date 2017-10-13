c493603057d4f4cb3ab54d658cc818dbe4fde44b
furusiyya <billal.ariif@gmail.com>
2017-10-13 15:21:01 +0500

    crowdfunding contract support whitelisting, critical bug fixed in Token contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,5 +1,8 @@
 /**
- *  Crowdfunding.sol v1.0.0
+ *  Crowdfunding.sol v1.1.0
+ *  V1.1.0 accept only whitelisted contributions
+ *  So only those addresses are whitelisted who satisfies
+ *  KYC standards.
  * 
  *  Bilal Arif - https://twitter.com/furusiyya_
  *  Notary Platform
@@ -22,7 +25,7 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
       uint256 private endsAt;
     
       /* how many token units a buyer gets per wei */
-      uint256 public rate;
+      uint256 private rate;
     
       /* How many wei of funding we have received so far */
       uint256 private weiRaised = 0;
@@ -46,6 +49,8 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
       /** How much ETH each address has invested to this crowdsale */
       mapping (address => uint256) private investedAmountOf;
     
+      /** Whitelisted addresses */
+      mapping (address => bool) private whiteListed;
       
       /** State machine
        *
@@ -69,38 +74,27 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
       // Crowdsale end time has been changed
       event EndsAtChanged(uint256 endTimestamp);
     
+      event NewExchangeRate(uint256 indexed _rate);
       event TokenContractAddress(address indexed oldAddress,address indexed newAddress);
       event TokenStoreUpdated(address indexed oldAddress,address indexed newAddress);
       event WalletAddressUpdated(address indexed oldAddress,address indexed newAddress);
-    
-      function Crowdfunding(
-          address frOwner,
-          uint256 startTimestamp, 
-          uint256 endTimestamp, 
-          address multiSigAddr, 
-          address tokenAddr,
-          address _tokenStore,
-          uint256 _rate) 
+      event WhiteListUpdated(address indexed investor, bool status);
+      event BonusesUpdated(address indexed investor, bool status);
+
+      function Crowdfunding() 
+      Ownable(0x0587e235a5906ed8143d026dE530D77AD82F8A92)
       {
+        require(earlyBirds());       // load bonuses
         
-        require(frOwner != 0x00);
-        require(multiSigAddr != 0x00);
-        require(_tokenStore != 0x00);
-        require(tokenAddr != 0);
-        require(startTimestamp >= now); 
-        require(endTimestamp  >= startTimestamp); 
-        require(_rate > 0);
+        multiSig = 0x1D1739F37a103f0D7a5f5736fEd2E77DE9863450;
+        tokenStore = 0x244092a2FECFC48259cf810b63BA3B3c0B811DCe;
         
-        owner = frOwner;
-        multiSig = multiSigAddr;
-        tokenStore = _tokenStore;
-        
-        token = NotaryPlatformToken(tokenAddr);
+        token = NotaryPlatformToken(0xbA5787e07a0636A756f4B4d517b595dbA24239EF);
         require(token.isTokenContract());
     
-        startsAt = startTimestamp;
-        endsAt = endTimestamp;
-        rate = _rate;
+        startsAt = now + 2 minutes;
+        endsAt = now + 31 days;
+        rate = 2730;
       }
     
       /**
@@ -121,7 +115,8 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
        */
       function buy(address receiver) public whenNotPaused nonReentrant inState(State.Funding) nonZero payable returns(bool){
         require(receiver != 0x00);
-        
+        require(whiteListed[receiver] || isEarlyBird(receiver));
+
         if(investedAmountOf[msg.sender] == 0) {
           // A new investor
           investorCount++;
@@ -136,8 +131,10 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
         // Up total accumulated fudns
         weiRaised = weiRaised.add(msg.value);
         
+        uint256 value = getBonus(receiver,msg.value);
+        
         // calculate token amount to be transfered
-        uint256 tokens = msg.value.mul(rate);
+        uint256 tokens = value.mul(rate);
         
         // Transfer NTRY tokens to receiver address
         if(!token.transferFrom(tokenStore,receiver,tokens)){
@@ -226,6 +223,21 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
       function fundingRaised() external constant returns (uint256){
         return weiRaised;
       }
+
+      /**
+       * @return exchage rate of ethers to NTRY tokens 
+       */
+      function exchnageRate() external constant returns (uint256){
+        return rate;
+      }
+
+      /**
+       * @return the status of the contract if it is allowed to participate. 
+       */
+      function isWhiteListed(address _address) external constant returns(bool){
+        require(_address != 0x00);
+        return whiteListed[_address];
+      }
       
       /**
        * Crowdfund state machine management.
@@ -283,7 +295,7 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
        * but we trust owners know what they are doing.
        *
        */
-      function updateEndsAt(uint256 _endsAt) onlyOwner {
+      function updateEndsAt(uint256 _endsAt) external  onlyOwner {
         
         // Don't change past
         require(_endsAt > now);
@@ -291,7 +303,25 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
         endsAt = _endsAt;
         EndsAtChanged(_endsAt);
       }
+
+      /**
+       * Allow crowdsale owner to change exchange rate.
+       */
+      function updateExchangeRate(uint256 _newRate) external onlyOwner {
+        
+        // Don't change past
+        require(_newRate > 0);
     
+        rate = _newRate;
+        NewExchangeRate(_newRate);
+      }
+
+      function updateWhiteList(address _address,bool _status) external onlyOwner returns(bool){
+        require(_address != 0x00);
+        whiteListed[_address] = _status;
+        WhiteListUpdated(_address, _status);
+        return true;
+      }
     
     
       /** Interface marker. */
@@ -310,10 +340,124 @@ contract Crowdfunding is Pausable, ReentrancyGuard {
     
       /** Modifier allowing execution only if received value is greater than zero */
       modifier nonZero(){
-        require(msg.value > 0);
+        require(msg.value >= 75000000000000000);
         _;
       }
-    }
+
+
+      //////////////////////////////////// Bonuses ////////////////////////////////
+
+      mapping (address => bool) private bonuses;
+
+      function earlyBirds() private returns(bool){
+        bonuses[0x017ABCC1012A7FfA811bBe4a26804f9DDac1Af4D] = true;
+        bonuses[0x1156ABCBA63ACC64162b0bbf67726a3E5eA1E157] = true;
+        bonuses[0xEAC8483261078517528DE64956dBD405f631265c] = true;
+        bonuses[0xB0b0D639b612937D50dd26eA6dc668e7AE51642A] = true;
+        bonuses[0x417535DEF791d7BBFBC97b0f743a4Da67fD9eC3B] = true;
+        bonuses[0x6723f81CDc9a5D5ef2Fe1bFbEdb4f83Bd017D3dC] = true;
+        bonuses[0xb9Bd4f154Bb5F2BE5E7Db0357C54720c7f35405d] = true;
+        bonuses[0x21CA5617f0cd02f13075C7c22f7231D061F09189] = true;
+        bonuses[0x0a6Cd7e558c69baF7388bb0B3432E29Ecc29ac55] = true;
+        bonuses[0x6a7f63709422A986A953904c64F10D945c8AfBA1] = true;
+        bonuses[0x7E046CB5cE19De94b2D0966B04bD8EF90cDC35d3] = true;
+        bonuses[0x1C3118b84988f42007c548e62DFF47A12c955886] = true;
+        bonuses[0x7736154662ba56C57B2Be628Fe0e44A609d33Dfb] = true;
+        bonuses[0xCcC8d4410a825F3644D3a5BBC0E9dF4ac6B491B3] = true;
+        bonuses[0x9Eff6628545E1475C73dF7B72978C2dF90eDFeeD] = true;
+        bonuses[0x235377dFB1Da49e39692Ac2635ef091c1b1cF63A] = true;
+        bonuses[0x6a8d793026BeBaef1a57e3802DD4bB6B1C844755] = true;
+        bonuses[0x26c32811447c8D0878b2daE7F4538AE32de82d57] = true;
+        bonuses[0x9CEdb0e60B3C2C1cd9A2ee2E18FD3f68870AF230] = true;
+        bonuses[0x28E102d747dF8Ae2cBBD0266911eFB609986515d] = true;
+        bonuses[0x5b35061Cc9891c3616Ea05d1423e4CbCfdDF1829] = true;
+        bonuses[0x47f2404fa0da21Af5b49F8E011DF851B69C24Aa4] = true;
+        bonuses[0x046ec2a3a16e76d5dFb0CFD0BF75C7CA6EB8A4A2] = true;
+        bonuses[0x01eD3975993c8BebfF2fb6a7472679C6F7b408Fb] = true;
+        bonuses[0x011afc4522663a310AF1b72C5853258CCb2C8f80] = true;
+        bonuses[0x3A167819Fd49F3021b91D840a03f4205413e316B] = true;
+        bonuses[0xd895E6E5E0a13EC2A16e7bdDD6C1151B01128488] = true;
+        bonuses[0xE5d4AaFC54CF15051BBE0bA11f65dE4f4Ccedbc0] = true;
+        bonuses[0x21C4ff1738940B3A4216D686f2e63C8dbcb7DC44] = true;
+        bonuses[0x196a484dB36D2F2049559551c182209143Db4606] = true;
+        bonuses[0x001E0d294383d5b4136476648aCc8D04a6461Ae3] = true;
+        bonuses[0x2052004ee9C9a923393a0062748223C1c76a7b59] = true;
+        bonuses[0x80844Fb6785c1EaB7671584E73b0a2363599CB2F] = true;
+        bonuses[0x526127775D489Af1d7e24bF4e7A8161088Fb90ff] = true;
+        bonuses[0xD4340FeF5D32F2754A67bF42a44f4CEc14540606] = true;
+        bonuses[0x51A51933721E4ADA68F8C0C36Ca6E37914A8c609] = true;
+        bonuses[0xD0780AB2AA7309E139A1513c49fB2127DdC30D3d] = true;
+        bonuses[0xE4AFF5ECB1c686F56C16f7dbd5d6a8Da9E200ab7] = true;
+        bonuses[0x04bC746A174F53A3e1b5776d5A28f3421A8aE4d0] = true;
+        bonuses[0x0D5f69C67DAE06ce606246A8bd88B552d1DdE140] = true;
+        bonuses[0x8854f86F4fBd88C4F16c4F3d5A5500de6d082AdC] = true;
+        bonuses[0x73c8711F2653749DdEFd7d14Ab84b0c4419B91A5] = true;
+        bonuses[0xb8B0eb45463d0CBc85423120bCf57B3283D68D42] = true;
+        bonuses[0x7924c67c07376cf7C4473D27BeE92FE82DFD26c5] = true;
+        bonuses[0xa6A14A81eC752e0ed5391A22818F44aA240FFBB1] = true;
+        bonuses[0xdF88295a162671EFC14f3276A467d31a5AFb63AC] = true;
+        bonuses[0xC1c113c60ebf7d92A3D78ff7122435A1e307cE05] = true;
+        bonuses[0x1EAaD141CaBA0C85EB28E0172a30dd8561dde030] = true;
+        bonuses[0xDE3270049C833fF2A52F18C7718227eb36a92323] = true;
+        bonuses[0x2348f7A9313B33Db329182f4FA78Bc0f94d2F040] = true;
+        bonuses[0x07c9CC6C24aBDdaB4a7aD82c813b059DD04a7F07] = true;
+        bonuses[0xd45BF2dEBD1C4196158DcB177D1Ae910949DC00A] = true;
+        bonuses[0xD1F3A1A16F4ab35e5e795Ce3f49Ee2DfF2dD683B] = true;
+        bonuses[0x6D567fa2031D42905c40a7E9CFF6c30b8DA4abf6] = true;
+        bonuses[0x4aF3b3947D4b4323C241c99eB7FD3ddcAbaef0d7] = true;
+        bonuses[0x386167E3c00AAfd9f83a89c05E0fB7e1c2720095] = true;
+        bonuses[0x916F356Ccf821be928201505c59a44891168DC08] = true;
+        bonuses[0x47cb69881e03213D1EC6e80FCD375bD167336621] = true;
+        bonuses[0x36cFB5A6be6b130CfcEb934d3Ca72c1D72c3A7D8] = true;
+        bonuses[0x1b29291cF6a57EE008b45f529210d6D5c5f19D91] = true;
+        bonuses[0xe6D0Bb9FBb78F10a111bc345058a9a90265622F3] = true;
+        bonuses[0x3e83Fc87256142dD2FDEeDc49980f4F9Be9BB1FB] = true;
+        bonuses[0xf360b24a530d29C96a26C2E34C0DAbCAB12639F4] = true;
+        bonuses[0xF49C6e7e36A714Bbc162E31cA23a04E44DcaF567] = true;
+        bonuses[0xa2Ac3516A33e990C8A3ce7845749BaB7C63128C0] = true;
+        bonuses[0xdC5984a2673c46B68036076026810FfDfFB695B8] = true;
+        bonuses[0xfFfdFaeF43029d6C749CEFf04f65187Bd50A5311] = true;
+        bonuses[0xe752737DD519715ab0FA9538949D7F9249c7c168] = true;
+        bonuses[0x580d0572DBD9F27C75d5FcC88a6075cE32924C2B] = true;
+        bonuses[0x6ee541808C463116A82D76649dA0502935fA8D08] = true;
+        bonuses[0xA68B4208E0b7aACef5e7cF8d6691d5B973bAd119] = true;
+        bonuses[0x737069E6f9F02062F4D651C5C8C03D50F6Fc99C6] = true;
+        bonuses[0x00550191FAc279632f5Ff23d06Cb317139543840] = true;
+        bonuses[0x9e6EB194E26649B1F17e5BafBcAbE26B5db433E2] = true;
+        bonuses[0x186a813b9fB34d727fE1ED2DFd40D87d1c8431a6] = true;
+        bonuses[0x7De8D937a3b2b254199F5D3B38F14c0D0f009Ff8] = true;
+        bonuses[0x8f066F3D9f75789d9f126Fdd7cFBcC38a768985D] = true;
+        bonuses[0x7D1826Fa8C84608a6C2d5a61Ed5A433D020AA543] = true;
+        return true;
+      }
+
+      function updateBonuses(address _address,bool _status) external onlyOwner returns(bool){
+        require(_address != 0x00);
+        bonuses[_address] = _status;
+        BonusesUpdated(_address,_status);
+        return true;
+      }
+
+      function getBonus(address _address,uint256 _value) private returns(uint256){
+        if(bonuses[_address]){
+           // 10% bonus
+           if(_value > 166 ether){
+            return (_value*11)/10;
+           }
+           // 7.5% bonus
+           if(_value > 33 ether){
+            return (_value*43)/40;
+           }
+           return (_value*21)/20;
+        }
+        return _value;
+      }
+
+      function isEarlyBird(address _address) constant returns(bool){
+        require(_address != 0x00);
+        return bonuses[_address];
+      }
+}
 
 contract NotaryPlatformToken{
     function isTokenContract() returns (bool);
--- contracts/NotaryPlatformToken.sol
+++ contracts/NotaryPlatformToken.sol
@@ -1,5 +1,5 @@
 /**
- *  NotaryPlatformToken.sol v1.0.0
+ *  NotaryPlatformToken.sol v1.0.1
  * 
  *  Bilal Arif - https://twitter.com/furusiyya_
  *  Notary Platform
@@ -13,14 +13,16 @@ import './Pausable.sol';
 import './Allocations.sol';
 import './ReentrancyGuard.sol';
 
+
 contract NotaryPlatformToken is Pausable, Allocations, ReentrancyGuard{
 
   using SafeMath for uint256;
 
-  string constant name = "Notary Platform Token";
-  string constant symbol = "NTRY";
-  uint8 constant decimals = 18;
-  uint256 totalSupply = 150000000 * 1 ether;
+  string constant public name = "Notary Platform Token";
+  string constant public symbol = "NTRY";
+  uint8 constant public decimals = 18;
+  uint256 public totalSupply = 150000000 * 1 ether;
+  string constant version = "v1.0.1";
 
   mapping(address => uint256) private balances;
   mapping (address => mapping (address => uint256)) private allowed;
@@ -28,9 +30,10 @@ contract NotaryPlatformToken is Pausable, Allocations, ReentrancyGuard{
   event Approval(address indexed owner, address indexed spender, uint256 value);
   event Transfer(address indexed from, address indexed to, uint256 value);
 
-  function NotaryPlatformToken() Ownable(msg.sender){
+  function NotaryPlatformToken() Ownable(0x1538EF80213cde339A333Ee420a85c21905b1b2D){
     // Allocate initial balance to the owner //
-    balances[owner] = 134875000 * 1 ether;  
+    balances[0x244092a2FECFC48259cf810b63BA3B3c0B811DCe] = 134875000 * 1 ether;
+    require(ICOParticipants(0x244092a2FECFC48259cf810b63BA3B3c0B811DCe));
   }
 
 
@@ -208,13 +211,56 @@ contract NotaryPlatformToken is Pausable, Allocations, ReentrancyGuard{
    * http://vessenes.com/the-erc20-short-address-attack-explained/
    */
   modifier onlyPayloadSize(uint size) {
-     require(msg.data.length > size + 4);
+     require(msg.data.length == size + 4);
      _;
   }
 
   function () {
     //if ether is sent to this address, send it back.
     revert();
+  }
+  
+  
+
+   function ICOParticipants(address _supplyOwner) private returns(bool){
+        /**
+         * Adresses who participated in first day of ICO and got first version of
+         * token. They will automatically get this latest version of token.
+         * ICO Contract: https://etherscan.io/address/0x34a3deb32b4705018f1e543a5867cf01aff3f15b
+         * Latest transaction hash: https://etherscan.io/tx/0x1d179fb045a86eed7a78e2e247c0822fc43f1a163f893996f88fdccd455d515b
+        */
+        balances[0xd0780ab2aa7309e139a1513c49fb2127ddc30d3d] = 15765750000000000000000;
+        balances[0x196a484db36d2f2049559551c182209143db4606] = 2866500000000000000000;
+        balances[0x36cfb5a6be6b130cfceb934d3ca72c1d72c3a7d8] = 28665000000000000000000;
+        balances[0x21c4ff1738940b3a4216d686f2e63c8dbcb7dc44] = 2866500000000000000000;
+        balances[0xd1f3a1a16f4ab35e5e795ce3f49ee2dff2dd683b] = 1433250000000000000000;
+        balances[0xd45bf2debd1c4196158dcb177d1ae910949dc00a] = 5733000000000000000000;
+        balances[0xdc5984a2673c46b68036076026810ffdffb695b8] = 1433250000000000000000;
+        balances[0x6ee541808c463116a82d76649da0502935fa8d08] = 57330000000000000000000;
+        balances[0xde3270049c833ff2a52f18c7718227eb36a92323] = 4948241046840000000000;
+        balances[0x51a51933721e4ada68f8c0c36ca6e37914a8c609] = 17199000000000000000000;
+        balances[0x737069e6f9f02062f4d651c5c8c03d50f6fc99c6] = 2866500000000000000000;
+        balances[0xa6a14a81ec752e0ed5391a22818f44aa240ffbb1] = 2149875000000000000000;
+        balances[0xeac8483261078517528de64956dbd405f631265c] = 11466000000000000000000;
+        balances[0x7736154662ba56c57b2be628fe0e44a609d33dfb] = 2866500000000000000000;
+        balances[0xc1c113c60ebf7d92a3d78ff7122435a1e307ce05] = 5733000000000000000000;
+        balances[0xfffdfaef43029d6c749ceff04f65187bd50a5311] = 2293200000000000000000;
+        balances[0x8854f86f4fbd88c4f16c4f3d5a5500de6d082adc] = 2866500000000000000000;
+        balances[0x26c32811447c8d0878b2dae7f4538ae32de82d57] = 2436525000000000000000;
+        balances[0xe752737dd519715ab0fa9538949d7f9249c7c168] = 2149875000000000000000;
+        balances[0x01ed3975993c8bebff2fb6a7472679c6f7b408fb] = 11466000000000000000000;
+        balances[0x7924c67c07376cf7c4473d27bee92fe82dfd26c5] = 11466000000000000000000;
+        balances[0xf360b24a530d29c96a26c2e34c0dabcab12639f4] = 8599500000000000000000;
+        balances[0x6a7f63709422a986a953904c64f10d945c8afba1] = 2866500000000000000000;
+        balances[0xa68b4208e0b7aacef5e7cf8d6691d5b973bad119] = 2149875000000000000000;
+        balances[0xb9bd4f154bb5f2be5e7db0357c54720c7f35405d] = 2149875000000000000000;
+        balances[0x6723f81cdc9a5d5ef2fe1bfbedb4f83bd017d3dc] = 5446350000000000000000;
+        balances[0x8f066f3d9f75789d9f126fdd7cfbcc38a768985d] = 146737500000000000000000;
+        balances[0xf49c6e7e36a714bbc162e31ca23a04e44dcaf567] = 25769835000000000000000;
+        balances[0x1538ef80213cde339a333ee420a85c21905b1b2d] = 2730000000000000000000;
+        balances[0x81a837cc83b55a67351c1070920f061dda307348] = 25511850000000000000000;
+        balances[_supplyOwner] -= 417961751000000000000000;
+        return true;
   }
 
 }
